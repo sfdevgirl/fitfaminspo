35d3d4142543052cc712ba221c8eb78c34e757ba
Shannon Smith <shannones2013@gmail.com>
2015-08-26 12:52:28 -0700

    ajax start

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ $(document).ready( function() {
 	$('.btn').on("click",function() {
 		$(".btn").hide();
 		$('.glass.hidden').show();
+		console.log(request);
 	});
 });
+
+var getImages = function(){
+	
+	$.ajax({
+		// url: "https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=07f9f44e86e044e2a5ac299b0cc85ac6",
+		url: "https://api.instagram.com/v1/tags/search?q=fitfam&access_token=07f9f44e86e044e2a5ac299b0cc85ac6"
+		// dataType: "jsonp",
+		// type: "GET",
+		})
+};
+
