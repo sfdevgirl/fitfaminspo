318d6c3aa67df78f5cc3150fbc6236575653c259
Shannon Smith <shannonesmith@Sfdevgirl.attlocal.net>
2015-09-09 11:40:37 -0700

    add comments

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ $(document).ready( function() {
 		$('h3.masthead-brand').css("position", "none")
 
 	});
+
 });
 const API_URL = "https://api.instagram.com/v1/tags/shannongetshealthyfit/media/recent?client_id=07f9f44e86e044e2a5ac299b0cc85ac6"
 //"https://api.instagram.com/v1/users/33885538/media/recent/?client_id=07f9f44e86e044e2a5ac299b0cc85ac6";
@@ -27,17 +28,33 @@ function getRequest(){
 
 function showResults(results){
   var html = "";
-  var cap = "";
+  
   $.each(results, function(index, value){
-  	
-     html += '<img src="' + value.images.standard_resolution.url + '"/>'; 
-     // html += '<figcaption>' + value.caption.text + '"</figcaption>'; 
-     // html += '<span>' + value.caption.text + '"</span>'; 
+  	html += '<div class="card">';
+    html += '<img src="' + value.images.standard_resolution.url + '"/>'; 
+    html += '<figcaption>' + value.caption.text + '"</figcaption>'; 
+    html += '</div>';
     
+//     function getRequestTwo(){
+	
+// 	var request = $.ajax({ 
+// 		url: pagination.next_url, 
+// 		type: "GET",
+// 		dataType: "JSONP" });
+// 	request.success(function(data){
+// 		showResults(data.data);
 
+// 	});
+// };
+    
+// $('.more').on("click", function(){
+// 	getRequestTwo();
+// });
     
   });
   $('.inner .glass .form').html(html);
-  $('.inner .glass .form .caption').html(cap);
+  // $('.inner .glass .form .caption').html(cap);
 }
 
+
+	
